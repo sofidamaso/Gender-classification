bcc0e13d550de861dcc0610a5e93ea94c7ef1678
Sofia Damaso <57020694+sofidamaso@users.noreply.github.com>
2023-07-02 21:04:37 +0200

    fixed error in cvpartition

--- main.m
+++ main.m
@@ -4,23 +4,23 @@ nimages = numel(images);
 lbp =[];
  for n = 1 : nimages
     im = imread(['gender-feret-master-union/gender-feret-master/' images{n}]);
-    lbp  = [lbp;compute_lbp(im)];
+    lbp = [lbp;compute_lbp(im)];
  end
 
 % Partizionamento dei dati
 % prima partizione 20% e 80%
-cv1= cvpartition(labels,'Holdout',0.2);
-cv1_trainLabels= labels(cv1.training);
-cv1_trainValues= lbp(cv1.training,:);
-cv1_testLabels= labels(cv1.test);
-cv1_testValues= lbp(cv1.test,:);
+cv1 = cvpartition(labels,'Holdout',0.2);
+cv1_trainLabels = labels(cv1.training);
+cv1_trainValues = lbp(cv1.training,:);
+cv1_testLabels = labels(cv1.test);
+cv1_testValues = lbp(cv1.test,:);
 
 %seconda partizione 20% e 80%
-cv2= cvpartition(cv1_trainLabels,'Holdout',0.2);
-cv2_trainLabels= labels(cv2.training);
-cv2_trainValues= lbp(cv2.training,:);
-cv2_validationLabels= labels(cv2.test);
-cv2_validationValues= lbp(cv2.test,:);
+cv2 = cvpartition(cv1_trainLabels,'Holdout',0.2);              
+cv2_trainLabels = cv1_trainLabels(cv2.training);
+cv2_trainValues = cv1_trainValues(cv2.training,:);
+cv2_validationLabels = cv1_trainLabels(cv2.test);
+cv2_validationValues = cv1_trainValues(cv2.test,:);
 
 % stima del K migliore
 accuracy_result_eu= [];
