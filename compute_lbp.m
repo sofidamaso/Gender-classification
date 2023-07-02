169607899e4188de0c6a7658aea60025f51731c7
Sofia Damaso <57020694+sofidamaso@users.noreply.github.com>
2023-07-02 21:18:47 +0200

    added comment

--- compute_lbp.m
+++ compute_lbp.m
@@ -1,16 +1,12 @@
+% Questa funzione calcola il valore del LBP di una immagine fornita in
+% input
 function out=compute_lbp(image)
-
   if ~isinteger(image)
     error('L''immagine deve essere RGB');
   end
-
   ch = size(image,3);
-  
   if (ch==3) 
     image = rgb2gray(image);
   end
-
   out = extractLBPFeatures(image,'NumNeighbors',8,'Radius',1,'Upright',true);
-    
-
 end
